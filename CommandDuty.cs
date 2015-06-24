74e1c80124db2cacce512882c1a354dc4c4d2cdb
Ev1dentFir3 <Andrew.dillon1989@gmail.com>
2015-06-24 00:22:38 -0400

    Fixed Command
    Typos in the command text

---
+++ CommandDuty.cs
@@ -0,0 +1,48 @@
+﻿using Rocket.Unturned.Commands;
+using Rocket.Unturned.Player;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EFG
+{
+    public class CommandDuty : IRocketCommand
+    {
+        public void Execute(RocketPlayer caller, string[] command)
+        {
+            if (caller.IsAdmin) caller.Admin(false); else caller.Admin(true);
+            if (caller.IsAdmin){
+                caller.Admin(false);
+                caller.Features.GodMode = false;
+                caller.Features.VanishMode = false;
+            }
+            else{
+                caller.Admin(true);
+            }
+        }
+
+        public string Help
+        {
+            get { return "Let your staff do their job!"; }
+        }
+
+        public string Name
+        {
+            get { return "duty"; }
+        }
+
+        public string Syntax
+        {
+            get { return ""; }
+        }
+
+        public bool RunFromConsole
+        {
+            get { return false; }
+        }
+
+        public List<string> Aliases {
+            get { return new List<string>() { "d" }; }
+        }
+    }
+}
